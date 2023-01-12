6904b49b36aebb873e6486ce8ef6063960f80c58
Gabriel Xavier <gabrielgrx@gmail.com>
2023-01-12 09:28:03 -0300

    User insertion with POST

--- src/main/java/com/gabrielxavier/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielxavier/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.gabrielxavier.workshopmongo.services;
 
 import com.gabrielxavier.workshopmongo.domain.entities.User;
+import com.gabrielxavier.workshopmongo.dto.UserDTO;
 import com.gabrielxavier.workshopmongo.repositories.UserRepository;
 import com.gabrielxavier.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
