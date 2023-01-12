82fe8785cc16ae58be42a3d1bcf70cda15a0fb8c
Gabriel Xavier <gabrielgrx@gmail.com>
2023-01-12 09:17:56 -0300

    User findById

--- src/main/java/com/gabrielxavier/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielxavier/workshopmongo/services/UserService.java
@@ -2,10 +2,13 @@ package com.gabrielxavier.workshopmongo.services;
 
 import com.gabrielxavier.workshopmongo.domain.entities.User;
 import com.gabrielxavier.workshopmongo.repositories.UserRepository;
+import com.gabrielxavier.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +18,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
